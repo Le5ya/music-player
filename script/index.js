df00efc16b7d2016f42239eb0f89eb1a890011a2
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-11-04 17:34:12 +0300

    day-4

--- script/index.js
+++ script/index.js
@@ -1,25 +1,155 @@
+const dataMusic = [
+  {
+    id: '1',
+    artist: 'The weeknd',
+    track: 'Save your tears',
+    poster: 'img/photo-1.jpg',
+    mp3: 'The Weeknd-Save Your Tears.mp3',
+  },
+  {
+    id: '2',
+    artist: 'Imagine Dragons',
+    track: 'Follow You',
+    poster: 'img/photo-2.jpg',
+    mp3: 'audio/Imagine Dragons - Follow You.mp3',
+  },
+  {
+    id: '3',
+    artist: 'Tove Lo',
+    track: 'How Long',
+    poster: 'img/photo-3.jpg',
+    mp3: 'audio/Tove Lo - How Long.mp3',
+  },
+  {
+    id: '4',
+    artist: 'Tom Odell',
+    track: 'Another Love',
+    poster: 'img/photo-4.jpg',
+    mp3: 'audio/Tom Odell - Another Love.mp3',
+  },
+  {
+    id: '5',
+    artist: 'Lana Del Rey',
+    track: 'Born To Die',
+    poster: 'img/photo-5.jpg',
+    mp3: 'audio/Lana Del Rey - Born To Die.mp3',
+  },
+  {
+    id: '6',
+    artist: 'Adele',
+    track: 'Hello',
+    poster: 'img/photo-6.jpg',
+    mp3: 'audio/Adele - Hello.mp3',
+  },
+  {
+    id: '7',
+    artist: 'Tom Odell',
+    track: "Can't Pretend",
+    poster: 'img/photo-7.jpg',
+    mp3: "audio/Tom Odell - Can't Pretend.mp3",
+  },
+  {
+    id: '8',
+    artist: 'Lana Del Rey',
+    track: 'Young And Beautiful',
+    poster: 'img/photo-8.jpg',
+    mp3: 'audio/Lana Del Rey - Young And Beautiful.mp3',
+  },
+  {
+    id: '9',
+    artist: 'Adele',
+    track: 'Someone Like You',
+    poster: 'img/photo-9.jpg',
+    mp3: 'audio/Adele - Someone Like You.mp3',
+  },
+  {
+    id: '10',
+    artist: 'Imagine Dragons',
+    track: 'Natural',
+    poster: 'img/photo-10.jpg',
+    mp3: 'audio/Imagine Dragons - Natural.mp3',
+  },
+  {
+    id: '11',
+    artist: 'Drake',
+    track: 'Laugh Now Cry Later',
+    poster: 'img/photo-11.jpg',
+    mp3: 'audio/Drake - Laugh Now Cry Later.mp3',
+  },
+  {
+    id: '12',
+    artist: 'Madonna',
+    track: 'Frozen',
+    poster: 'img/photo-12.jpg',
+    mp3: 'audio/Madonna - Frozen.mp3',
+  },
+];
+
 const audio = new Audio();
 const tracksCard = document.getElementsByClassName('track');
+const catalogContainer = document.querySelector('.catalog__container');
 const player = document.querySelector('.player');
 const pauseBtn = document.querySelector('.player__controller_pause');
 const stopBtn = document.querySelector('.player__controller_stop');
+const prevBtn = document.querySelector('.player__controller_prev');
+const nextBtn = document.querySelector('.player__controller_next');
+const likeBtn = document.querySelector('.player__controller_like');
+const muteBtn = document.querySelector('.player__controller_mute');
+
+const catalogAddBtn = document.createElement('button');
+catalogAddBtn.classList.add('catalog__btn-add');
+
+catalogAddBtn.innerHTML = `
+<span>Увидеть всё</span>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z"/>
+          </svg> 
+`
+
+
+
+const pausePlayer = () => {
+  const trackActive = document.querySelector('.track_active');
+}
 
 const playMusic = event => {
-  const trackActive = event.currentTarget
+  event.preventDefault();
+  const trackActive = event.currentTarget;
+
+  if(trackActive.classList.contains('track_active')) {
+    pausePlayer(trackFctive);
+    return
+  }
+  let i = 0;
+  const id = trackActive.dataset.idTrack;
+
+  const track = dataMusic.find((item, index) => {
+    i = index;
+    return id === item.id;
+  });
+
+  audio.src = track.id
+
   audio.src = trackActive.dataset.track;
   audio.play();
   pauseBtn.classList.remove('player__icon_play');
   player.classList.add('player_active');
 
+  const prevTrack = i == 0 ? dataMusic.lehgth - 1 : i -1;
+  const nextTrack = i + 1 ===dataMusic.length ? 0 : i + 1;
+  prevBtn.dataset.idTrack = dataMusic[prevTrack].id;
+  nextBtn.dataset.idTrack = dataMusic[nextTrack].id;
+
   for(let i = 0; i < tracksCard.length; i++) {
-    tracksCard[i].classList.remove(track_active); 
+    tracksCard[i].classList.remove('track_active'); 
   }
-  trackActive.classList.add(track_active);
+  trackActive.classList.add('track_active');
 }
 
-for(let i = 0; i < tracksCard.length; i++) {
-  tracksCard[i].addEventListener('click', playMusic);
-  
+const addHandlerTrack = () => {
+  for(let i = 0; i < tracksCard.length; i++) {
+  tracksCard[i].addEventListener('click', playMusic); 
+  }
 }
 pauseBtn.addEventListener('click', () => {
 if(audio.paused) {
@@ -31,7 +161,63 @@ if(audio.paused) {
 }
 })
 stopBtn.addEventListener('click', () => {
-  player.style.display = 'none';
+  player.classList.remove('player_active');
   audio.src = '';
-})
+});
+
+
+
+const createCard = (data) => {
+  const card = document.createElement('a');
+  card.href = '#';
+  card.classList.add('catalog__item', 'track');
+  card.dataset.idTrack = data.id;
+
+  card.innerHTML = `
+  <div class="track__img-wrap">
+    <img class="track__poster" src="${data.poster}" 
+    alt="${data.artist} ${data.track}"
+    width="180"
+    height="180">
+  </div>
+  <div class="track__info track-info">
+    <p class="track-info__title">${data.track}</p>
+    <p class="track-info__artist">${data.artist}</p>
+  </div>
+  `;
+  return card;
+}
+
+const renderCatalog = (dataList) => {
+  catalogContainer.textContent = '';
+  const listCards = dataList.map(createCard);
+  // console.log(listCards);
+  catalogContainer.append(...listCards);
+  addHandlerTrack();
+};
+
+const checkCount = (i = 1) => {
+  tracksCard[0]
+  if(catalogContainer.clientHeight> tracksCard[0].clientHeight * 3) {
+    tracksCard[tracksCard.length - 1].style.display = 'none';
+    checkCount(i + 1);
+  } else if(i !== 1) {
+    catalogContainer.append(catalogAddBtn);
+    
+  };
+
+};
+
+const init = () => {
+  renderCatalog(dataMusic);
+  checkCount();
+
+  catalogAddBtn.addEventListener('click', () => {
+    [...trackCard].forEach((trackCard) => {
+      trackCard.style.display = '';
+      // catalogAddBtn.remove();
+    })
+  })
+};
+init();
 
